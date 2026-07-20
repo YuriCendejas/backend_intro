c60a9bcd7fd1489359e94b069d8bbc308219b9ad
Yuri Cendejas <yuricendejas@outlook.com>
2026-07-20 00:45:37 -0400

    Update app.js
    in line 7 , "/" empty for now as a health check for the curl test . to confirm my server is working

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express(); // creates express app
 
+app.use(express.json()); 
+
+app.get("/",(req,res) => { res.send("Server is working!");}); // or else it'll say cannot get/ on the "curl" test
+
 export default app;
