3bf77138e0aae7a0101308a8c6bd2a166530f155
Yuri Cendejas <yuricendejas@outlook.com>
2026-07-22 18:33:24 -0400

    user routes

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,7 @@
+import { Router } from 'express'; 
+/* routers the tool that helps us gets routes in  express,
+ express is the framwork that handles the routes */
+
+ const router = Router();
+ 
+ export default router;
