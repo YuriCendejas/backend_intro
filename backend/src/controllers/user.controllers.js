d78067ca23cf9654d7e6ead0a95e33c5bc4ab6b1
Yuri Cendejas <yuricendejas@outlook.com>
2026-07-25 22:02:10 -0400

    update user controllers

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -5,11 +5,30 @@
 
     if(!username || !password|| !email){
         return res.status(400).json({message: " All fields required"})
-    }
+    } // making sure everything is written or else an alert of " all fields are required" . 400 = client side error
 
-        
-    } catch (error) {
-    console.log("try again");    
+
+    const exists = await User.findOne({email: email.toLowerCase() });
+    if (exists) {
+        return res.status(400).json({message:"hmmm, looks like that user already exists"});
+    }// user already exists . 400 = client server error
+
+    const user = await User.create({
+        username,
+        password,
+        email
+    });
+
+    return res.status(201).json({ // 201 = means "ok"
+        message: " User created successfully",
+    });
+
+    catch (error){
+    console.error(error);
+    return res.status(500).json({ // 500 = server side error
+        message: "server-side error "
+    });
     }
     
- }
+ };
+export {registerUser};
