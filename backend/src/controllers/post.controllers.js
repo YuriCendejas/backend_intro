c42fde1c29686b1ddd954e43c46259024add64ae
Yuri Cendejas <yuricendejas@outlook.com>
2026-08-25 22:11:20 -0400

    crud update

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -16,13 +16,15 @@ const createPost = async (req,res) => {
         
     } catch (error) { 
         return res.status(500).json({
-            message:" internal server error",error});
+            message:" internal server error",error
+        });
 
-        
     }
-    const getPosts =async (req,res) => {
+    };
+
+    const getPosts = async (req,res) => {
         try {
-            const getPosts = await Post.find();
+            const posts = await Post.find();
             res.status(200).json(posts);
             
         } catch (error) {
@@ -32,7 +34,46 @@ const createPost = async (req,res) => {
             
         }
         
-    }
+    };
 
+    /* i believe keys are the value pairs of xyz,. fields inside [] .
+            filed 1 (x) is 'name', field 2 (y) is 'description' and filed 3 (z) is 'age' . ex : [name, description,age]*/
+    const updatePosts = async(req,res) => { 
+        try {
+            if (!Object.keys(req.body).length) {
+                return res.status(400).json({
+                    message: " No fileds provided for update"
+                });
+            } 
+            const post = await Post.findByIdAndUpdate(req.params.id,req.body,{new:true});
+            
+
+            if(!post)return res.status(404).json({message : 'Post not found '});
+            res.status(200).json({
+                message:"post updated successfully",post
+            });
+
+        } catch(error){
+    return res.status(500).json({
+      message: "Internal server error",error
+    });
+ }
 };
-export{createPost};
+ const deletePost = async (req,res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id); 
+        if(!deleted) return res.status(400).json({
+            message:"Post Not Found"
+        }) //just incase you cant find what you want to delete.
+        res.status(200).json({
+            message:"Post Deleted"}) // bc we were able to delete the post
+    } catch (error) {
+        res.status(500).json({
+            message:"Internal server Error",error
+        });
+        
+    }
+    
+ };
+
+export{createPost,getPosts,updatePosts,deletePost};
