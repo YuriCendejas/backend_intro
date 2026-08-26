60db4a6b577f3bf60e168a339326bf2957fbe69d
Yuri Cendejas <yuricendejas@outlook.com>
2026-08-25 22:19:28 -0400

    crud updates

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { createPost } from "../controllers/post.controllers.js";
+import { createPost, getPosts,updatePosts,deletePost } from "../controllers/post.controllers.js";
 const router = Router(); 
 
 router.route('/create').post(createPost);//when POST request comes to /create , run createPost controller
 router.route("/getPosts").get(getPosts); 
+router.route("/update/:id").patch(updatePosts); //:id <-- change the idea to a specific id number in postmon
+router.route("/delete/:id").delete(deletePost);
 export default router;
 
 
-/* for postmon : http://localhost:5010/api/posts/create */
