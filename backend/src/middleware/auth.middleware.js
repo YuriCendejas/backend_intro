b979285db5ebf9e21cac72800f6873f6f8bcf61e
YuriCendejas <yuricendejas@outlook.com>
2026-09-01 17:59:23 -0400

    get User Profile

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -13,8 +13,16 @@ if (!authHeader || !authHeader.startsWith("Bearer ")){
 return res.status(401).json({message:"no token provided"});
 // 401 bc its not authorized bc the client didnt prove who they were.
 }
+const token =authHeader.split("")[1];
+/*authHeader.split("")[1] basically is just saying cut the string at the space and
+give me the second part bc the index of the token would be 1 and "bearer" is index of 0*/
 
-        
+const decoded =jwt.verify(token,process.env.JWT_SECRET);
+//The verify part is to verify an existing token exist instead of "jwt.sign() "",that creates a new token when loging in for the first time
+        // checks if token was really OUR token and not just a faked one typed 
+        //has it expired yet ? from the JWT_EXPIRES_IN from env folder that we set to expire in a specifc timeframe 
+    
+        req,user
     } catch (error) {
         
     }
