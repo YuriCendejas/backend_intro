db8e0747c64812e6ba82fe2c8a5822a61ec2760a
YuriCendejas <yuricendejas@outlook.com>
2026-09-09 18:31:53 -0400

    updating middleware auth

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -18,12 +18,23 @@ const token =authHeader.split("")[1];
 give me the second part bc the index of the token would be 1 and "bearer" is index of 0*/
 
 const decoded =jwt.verify(token,process.env.JWT_SECRET);
-//The verify part is to verify an existing token exist instead of "jwt.sign() "",that creates a new token when loging in for the first time
+//The verify part is to verify an existing token exist instead of "jwt.sign()",that creates a new token when loging in for the first time
         // checks if token was really OUR token and not just a faked one typed 
         //has it expired yet ? from the JWT_EXPIRES_IN from env folder that we set to expire in a specifc timeframe 
     
-        req,user
+        req.user= decoded;
+ /*decoded = unlocked . 
+ , so whatever was inside the token when we created it like id and email,
+   attching it to "req.user" makes it available in whatever controller runs next*/
+
+   next(); 
+   // ok so the token is vaild , you can keep going on your request to the actual route ( like a getUserprofile )
+
+
     } catch (error) {
-        
+        return res.status(401).json({message:"Invaild or expired token"});
+        // if jwt.verify() doesnt verify what we need either bc of it being invaild token or expired , catch it .
     }
-}
+};
+
+export { protect };
