f64fd4a2a0e81f406582eaa54bf3cb96735efc6a
YuriCendejas <yuricendejas@outlook.com>
2026-09-21 15:02:02 -0400

    importing protect from middleware

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { createPost, getPosts,updatePosts,deletePost } from "../controllers/post.controllers.js";
+import {protect} from "../middleware/auth.middleware.js";
 const router = Router(); 
 
-router.route('/create').post(createPost);//when POST request comes to /create , run createPost controller
-router.route("/getPosts").get(getPosts); 
-router.route("/update/:id").patch(updatePosts); //:id <-- change the idea to a specific id number in postmon
-router.route("/delete/:id").delete(deletePost);
+router.route('/create').post(createPost);//when POST request comes to /create , run createPost controller. must be logged in to create a post
+router.route("/getPosts").get(getPosts); // anyone can view posts ,no login required
+router.route("/update/:id").patch(updatePosts); //:id <-- change the idea to a specific id number in postmon . must be logged in to update
+router.route("/delete/:id").delete(deletePost);//must be logged in to delete
 export default router;
 
 
